88895e03db31b2ef5386679e572ef0e2afefac1f
ingridhoffman <ingridhoffman.pdx@gmail.com>
2021-04-20 15:06:52 -0700

    initialization tests

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 # Gilded Rose Refactoring Kata
 
 ## Javascript - Jest
+
+### Add the missing tests
+
+1. Initial - create item, create shop with items, create shop without items
+
+### Refactor the code
+
+### Implement the new feature
--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,34 @@
-const {Shop, Item} = require("../src/gilded_rose");
+/** @format */
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
-  });
+const { Shop, Item } = require("../src/gilded_rose");
+
+// per instructions, Item and Items property are not modifiable
+// so I am only writing tests for these objects that will pass on current code
+
+// tests for 'Item'
+describe("ThingOne", function () {
+	it("should create an item with 'name', 'sellIn', and 'quality' properties", function () {
+		const thingOne = new Item("foo", 0, 0);
+		expect(thingOne.name).toBe("foo");
+		expect(thingOne.sellIn).toBe(0);
+		expect(thingOne.quality).toBe(0);
+	});
+});
+
+// tests for 'Shop'
+describe("Gilded Rose", function () {
+	describe("initialization", function () {
+		it("should create a shop with an array of items that have 'name', 'sellIn', and 'quality' properties", function () {
+			const gildedRose = new Shop([new Item("foo", 0, 0)]);
+			expect(gildedRose.items[0].name).toBe("foo");
+			expect(gildedRose.items[0].sellIn).toBe(0);
+			expect(gildedRose.items[0].quality).toBe(0);
+		});
+
+		// shop may be empty so allowing for not providing items in initializing
+		it("should create an empty shop if no items provided", function () {
+			const gildedRose = new Shop();
+			expect(gildedRose.items.length).toBe(0);
+		});
+	});
 });
